2735f24ce5c380ecfcf906d89f7ebf3d92228c37
Will Chou <wchou@inmusicbrands.com>
2019-06-11 15:29:34 +1200

    Moved clean_docker.sh to folder test

--- clean_docker.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-containers=$(docker container ls -a | grep -v IMAGE | grep -v ubuntu | awk '{print $1}')
-[ -z "$containers" ] || docker container rm $containers
-
-images=$(docker images | grep -v TAG | grep -v ubuntu | awk '{print $3}')
-[ -z "$images" ] || docker image rm $images
